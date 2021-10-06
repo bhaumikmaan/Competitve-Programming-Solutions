84e3951fde5b2eeacb7e3a68f8ec14440ee8b7d7
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-06 12:03:12 +0530

    Tags- Maths | DP

---
+++ LeetCode/70. Climbing Stairs.cpp
@@ -0,0 +1,19 @@
+/***	KNOWLEDGE IS POWER	***/
+
+// Problem Link: https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 2) 
+          return n;
+        int prev = 2, prev2 = 1, res ;
+        for (int i = 3; i <= n; i++) 
+        {
+            res = prev + prev2 ;
+            prev2 = prev ;
+            prev = res ;
+        }
+        return res ;
+    }
+};
