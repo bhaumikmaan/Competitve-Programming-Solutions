452b2bc4cde93117f8f69cfa7115ee2cd935a40e
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-06 12:05:54 +0530

    Create A - Your First Judge.cpp

---
+++ AtCoder/ABC 215 - AtCoder Beginner Contest 215/A - Your First Judge.cpp
@@ -0,0 +1,34 @@
+/***	KNOWLEDGE IS POWER	***/
+
+// Problem Link: https://atcoder.jp/contests/abc215/tasks/abc215_a
+// Solution Link: https://atcoder.jp/contests/abc215/submissions/25191685
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    // cin >> testcases ;
+    while( testcases -- )
+    {
+        string s ;
+        cin >> s ;
+        if(s == "Hello,World!")
+        {
+        	cout << "AC" ;
+        }
+        else
+        {
+        	cout << "WA" ;
+        }
+        cout << "\n" ;
+    }
+    return 0 ;
+}    
