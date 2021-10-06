02314f9b0af6508837c20d5ceeb18fc57a68bd0c
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-06 12:15:08 +0530

    Format For Contribution

---
+++ Codeforces/Z Format.cpp
@@ -0,0 +1,20 @@
+/***	KNOWLEDGE IS POWER	***/
+
+// Problem Link: 
+// Solution Link:
+
+/*
+
+Your Solution Here
+
+*/
+//<---END HERE--->
+
+// EXTRA INFO:
+// In commit file option below- Mention the tags of the question
+
+/* 
+
+Naming Guide -> Codeforces -> (Folder) 1579 - Round #744 (Div. 3) -> (Question) A. Casimir's String Solitaire
+
+*/
