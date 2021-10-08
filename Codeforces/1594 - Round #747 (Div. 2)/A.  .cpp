09ff84f136a6bc207a8be2f39e82f68b8fb4758e
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-08 20:50:28 +0530

    Update A.  .cpp

--- Codeforces/1594 - Round #747 (Div. 2)/A.  .cpp
+++ Codeforces/1594 - Round #747 (Div. 2)/A.  .cpp
@@ -1,3 +1,21 @@
 /***  KNOWLEDGE IS POWER  ***/
 
+// PROBLEM STATEMENT
+
+Theofanis has a riddle for you and if you manage to solve it, he will give you a Cypriot snack halloumi for free (Cypriot cheese).
+
+You are given an integer n. You need to find two integers l and r such that −1018≤l<r≤1018 and l+(l+1)+…+(r−1)+r=n.
+
+Input
+The first line contains a single integer t (1≤t≤104) — the number of test cases.
+
+The first and only line of each test case contains a single integer n (1≤n≤1018).
+
+Output
+For each test case, print the two integers l and r such that −10^18≤l<r≤10^18 and l+(l+1)+…+(r−1)+r=n.
+
+It can be proven that an answer always exists. If there are multiple answers, print any.
+
+
+
 SOLUTION SOON :)
