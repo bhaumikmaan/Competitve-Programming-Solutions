263c42d0290e3e865f8207c67f49792122a4cfa3
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-10 20:15:55 +0530

    Create 201. Bitwise AND of Numbers Range.cpp

---
+++ LeetCode/201. Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,17 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        if ((m == 0) || (n == 0)) 
+            return 0;
+        if ((int)log2(m) != (int)log2(n)) 
+            return 0;
+        int res = m ;
+        for (long i = m; i <= n; i++)
+            res &= i ;
+                
+        return res ;
+
+    }
+};
