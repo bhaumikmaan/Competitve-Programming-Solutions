fdee6bf48b7070674c67e34e1ea981d3d64e44f7
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-14 09:53:19 +0530

    Create 279. Perfect Squares.cpp

---
+++ LeetCode/279. Perfect Squares.cpp
@@ -0,0 +1,17 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1);
+        for(int i=1; i<=n; i++) 
+        {
+            dp[i] = i ;
+            for(int j = 2; j*j <= i; j++) 
+            {
+                dp[i] = min(dp[i] , dp[i - j*j] + 1) ;
+            }
+        }
+        return dp[n] ;
+    }
+};
