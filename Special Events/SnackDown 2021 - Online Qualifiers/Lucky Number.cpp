4f6df7e28d0ec3d74cc24008a89b5e5e4517b8c5
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-15 19:58:46 +0530

    Create Lucky Number.cpp

---
+++ Special Events/SnackDown 2021 - Online Qualifiers/Lucky Number.cpp
@@ -0,0 +1,21 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+// PROBLEM STATEMENT
+// Chef buys a lottery ticket that has a 3 digit code associated with it. He thinks that digit 7 is his lucky digit and brings him good luck. 
+// Chef will win some amount in the lottery if at least one of the digits of the lottery ticket is 7.
+// Given three digits A, B, and C of the lottery ticket, tell whether Chef wins something or not?
+
+// SAMPLE INPUT 
+// 3
+// 0 0 0
+// 7 8 9
+// 2 7 7
+
+// SAMPLE OUTPUT
+// NO
+// YES
+// YES
+
+// SOLUTION
+
+STAR THE REPO AND FOLLOW
