d6a5a8c24362b2338290242527b21240d54649ef
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-15 20:01:33 +0530

    Create Maximum Number Of Distinct Elements.cpp

---
+++ Special Events/SnackDown 2021 - Online Qualifiers/Maximum Number Of Distinct Elements.cpp
@@ -0,0 +1,24 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+// PROBLEM STATEMENT
+// Given an array B of length N, find an array A of length N where 0≤Ai≤10^9 such that array C where Ci = Ai mod Bi, ∀i∈{1,N} has maximum number of distinct integers.
+
+// In case of multiple answers, print any.
+
+// SAMPLE INPUT 
+// 3
+// 3
+// 2 1 3
+// 2
+// 1 1
+// 6
+// 1 2 3 3 2 1
+
+// SAMPLE OUTPUT
+// 3 1 2
+// 2 3
+// 0 1 2 3 4 5
+
+// SOLUTION
+
+STAR THE REPO AND FOLLOW
