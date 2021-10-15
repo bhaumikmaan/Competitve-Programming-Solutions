3555752ab8ef325648ed741ee8129418f6854503
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-15 20:00:03 +0530

    Create Test Match Series.cpp

---
+++ Special Events/SnackDown 2021 - Online Qualifiers/Test Match Series.cpp
@@ -0,0 +1,22 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+// PROBLEM STATEMENT
+// A 5 match test series between India and England has just concluded.
+// Every match could have ended either as a win for India, a win for England, or a draw. You know the result of all the matches. 
+// Determine who won the series or if it ended in a draw.
+// A team is said to have won the series if it wins strictly more test matches than the other team.
+
+// SAMPLE INPUT 
+// 3
+// 0 1 2 1 0
+// 0 1 2 1 2
+// 2 2 2 2 1
+
+// SAMPLE OUTPUT
+// INDIA
+// DRAW
+// ENGLAND
+
+// SOLUTION
+
+STAR THE REPO AND FOLLOW
