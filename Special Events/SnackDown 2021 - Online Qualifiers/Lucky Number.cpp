6fd7e22baeaa665a065e5ac48550cbf76b43dec2
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-20 10:10:41 +0530

    Update Lucky Number.cpp

--- Special Events/SnackDown 2021 - Online Qualifiers/Lucky Number.cpp
+++ Special Events/SnackDown 2021 - Online Qualifiers/Lucky Number.cpp
@@ -18,4 +18,36 @@
 
 // SOLUTION
 
-STAR THE REPO AND FOLLOW
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+void solve( )
+{
+	int  a , b,  c;
+	cin >> a >> b >> c ;
+	if(a == 7 || b == 7 || c == 7)
+	{
+		cout << "YES" ;
+	}
+	else
+	{
+		cout << "NO" ;
+	}
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve() ;
+        cout << "\n" ;
+    }
+    return 0 ;
+}    
