128d5678b77966581170b23216a9f526a290e16c
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-24 00:23:54 +0530

    Update Round Robin Ranks.cpp

--- Special Events/SnackDown 2021 - Round 1A/Round Robin Ranks.cpp
+++ Special Events/SnackDown 2021 - Round 1A/Round Robin Ranks.cpp
@@ -1,1 +1,30 @@
 /***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+void solve( )
+{
+	int a , b ;
+	cin >> a >> b ;
+	int ans = (2*a - b - 1)/2 ;
+	ans *= 2 ;
+	cout << ans ;	
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve() ;
+        cout << "\n" ;
+    }
+    return 0 ;
+}    
