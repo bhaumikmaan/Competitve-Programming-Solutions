e3e961111dba1c7399533a18e095a88d440ba329
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-26 10:42:04 +0530

    Create Luntik and Concerts.cpp

---
+++ Codeforces/1582 - Round #750 (Div. 2)/Luntik and Concerts.cpp
@@ -0,0 +1,29 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+void solve( )
+{
+	int a , b , c ;
+	cin >> a >> b >> c ;
+	int ans = a + 2*b + 3*c ;
+	cout << ans%2 ;
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve() ;
+        cout << "\n" ;
+    }
+    return 0 ;
+}    
