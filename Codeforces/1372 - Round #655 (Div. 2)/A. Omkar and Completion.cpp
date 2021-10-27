0db8718a1560014777b5465b7b71804e57697631
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-27 12:22:39 +0530

    Create A. Omkar and Completion.cpp

---
+++ Codeforces/1372 - Round #655 (Div. 2)/A. Omkar and Completion.cpp
@@ -0,0 +1,23 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+using namespace std ;
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	int n ;
+    	cin >> n ;
+    	for(int i = 0 ; i < n ; i++)
+    	{
+    		cout << "1 " ;
+    	}
+    	cout << endl ;
+    }
+    return 0 ;
+}    
