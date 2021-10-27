16108fcb350c121291ea91fdaded5a0f856950f4
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-10-27 17:25:52 +0530

    Create C. Bits.cpp

---
+++ Codeforces/485 - Round #276 (Div. 2)/C. Bits.cpp
@@ -0,0 +1,35 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+void solve( )
+{
+	int l , r ;
+	cin >> l >> r ;
+	for(int i = 1 ; i + l <= r ; (i <<= 1))
+	{
+		if(!(l&i))
+		{
+			l += i ;
+		}
+	}    
+	cout << l ;
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve( ) ;
+        cout << "\n" ;
+    }
+    return 0 ;
+}    
