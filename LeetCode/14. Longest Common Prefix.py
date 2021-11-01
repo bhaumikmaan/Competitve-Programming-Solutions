b0825301b4d0a96b147620a3f6281b73bbe0ad73
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-01 19:31:12 +0530

    Create 14. Longest Common Prefix.py

---
+++ LeetCode/14. Longest Common Prefix.py
@@ -0,0 +1,17 @@
+### KNOWLEDGE IS POWER  ###
+
+class Solution(object):
+    def longestCommonPrefix(self, m):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not m: return ''
+				#since list of string will be sorted and retrieved min max by alphebetic order
+        s1 = min(m)
+        s2 = max(m)
+
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                return s1[:i] #stop until hit the split index
+        return s1
