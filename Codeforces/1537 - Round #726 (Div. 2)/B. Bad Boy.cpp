29240e5982a440a881242c7725b0bcf5dc6d3e22
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-01 19:21:04 +0530

    Create B. Bad Boy.cpp

---
+++ Codeforces/1537 - Round #726 (Div. 2)/B. Bad Boy.cpp
@@ -0,0 +1,25 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+        int n , m , x , y ;
+        cin >> n >> m >> x >> y ;
+        cout << n << " " << m << " " ;
+        cout << 1 << " " << 1 ; 
+        cout << endl ;
+    }
+    return 0 ;
+}    
