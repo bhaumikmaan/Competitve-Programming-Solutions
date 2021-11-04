d9ba487912ed7f922ea2a78e0a5afce8a49da3a8
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-04 12:13:18 +0530

    Create 404. Sum of Left Leaves.java

---
+++ LeetCode/404. Sum of Left Leaves.java
@@ -0,0 +1,32 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+		
+		// Checking if left Node is a leaf node
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right);
+        }
+
+		// Exploring the tree further.
+        return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+}
