1a608f6526c87efce54b2418ddbe99c1916b4d79
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-04 12:12:20 +0530

    Create 404. Sum of Left Leaves.cpp

---
+++ LeetCode/404. Sum of Left Leaves.cpp
@@ -0,0 +1,28 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root , bool is_left=false) {
+        if(!root)
+        {
+            return 0;
+        }
+        if(!root->left && !root->right)
+        {
+            if(is_left)
+                return root->val;
+        }
+        return sumOfLeftLeaves(root->left,true) + sumOfLeftLeaves(root->right,false);
+    }
+};
