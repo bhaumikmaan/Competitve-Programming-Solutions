45f27096f8f7d3d1624318e467c9f091559f667c
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-06 12:50:23 +0530

    Create D. Moderate Modular Mode.cpp

---
+++ Codeforces/1604 - Round #752 (Div. 2)/D. Moderate Modular Mode.cpp
@@ -0,0 +1,43 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 100000
+using namespace std ;
+void solve( )
+{
+	int x , y ;
+	cin >> x >> y ;
+	if(y%x)
+	{
+		if(y > x)
+		{
+			cout << y - ((y%x)/2) ;
+		}
+		else
+		{
+			cout << x + y ;
+		}
+	}
+	else
+	{
+		cout << x ;
+	}
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve( ) ;
+        cout << "\n" ;
+    }	
+    return 0 ;
+}    
