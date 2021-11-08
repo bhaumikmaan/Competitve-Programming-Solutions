7ae6e4a7de3912f6a8cc851c5dee4bf08f9818fd
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-08 19:40:44 +0530

    Update 96. Unique Binary Search Trees.cpp

--- LeetCode/96. Unique Binary Search Trees.cpp
+++ LeetCode/96. Unique Binary Search Trees.cpp
@@ -1,8 +1,13 @@
 /***  KNOWLEDGE IS POWER  ***/
 
-vector<int> dp(n+1);
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> dp(n+1);
         dp[0] = dp[1] = 1;
         for(int i = 2; i <= n; i++) 
             for(int j = 1; j <= i; j++)
                 dp[i] += dp[j-1] * dp[i-j];
         return dp[n];
+    }
+};
