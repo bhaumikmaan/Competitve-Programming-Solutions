d341f04cd3598186705db85092d27e500e8e62e1
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-08 19:39:55 +0530

    Create 96. Unique Binary Search Trees.cpp

---
+++ LeetCode/96. Unique Binary Search Trees.cpp
@@ -0,0 +1,8 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++) 
+            for(int j = 1; j <= i; j++)
+                dp[i] += dp[j-1] * dp[i-j];
+        return dp[n];
