7aac6c4bdfd11f95b37f9b38c62b15a82e6bc9b3
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-13 12:01:57 +0530

    Another Way

---
+++ Codeforces/1605 - Round #754 (Div. 2)/A. A.M. Deviation 2.cpp
@@ -0,0 +1,36 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 100010
+using namespace std ;
+void solve( )
+{
+	int a , b , c; 
+	cin >> a >> b >> c ;
+	if((a + b + c)%3 == 0)
+	{
+		cout << 0 ;
+	}
+	else
+	{
+		cout << 1 ;
+	}
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve( ) ;
+        cout << "\n" ;
+    }	
+    return 0 ;
+}    
