3f0bec21d9d0484485bf3721ef8ce2cb72114f7b
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-15 14:19:12 +0530

    Create A. Diverse Substring.cpp

---
+++ Codeforces/Educational Round 53 (Rated for Div. 2)/A. Diverse Substring.cpp
@@ -0,0 +1,40 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll  
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+using namespace std ;
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;		
+    // cin >> testcases ;
+    while( testcases -- )	
+    {
+    	int n ;
+    	cin >> n ;
+    	string s ;
+    	cin >> s ;
+    	bool flag = false ;
+    	for(int i = 0 ; i < n - 1 ; i++)
+    	{
+    		if(s[i] != s[i+1])
+    		{
+    			cout <<  "YES" << "\n" ;
+    			cout << s[i] << s[i+1] ;
+    			flag = true ;
+    			break ;
+    		}
+    	}
+    	if(!flag)
+    	{
+    		cout << "NO" ;
+    	}
+    	cout << "\n" ;
+    }
+    return 0 ;
+}    
