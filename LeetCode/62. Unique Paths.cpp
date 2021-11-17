09c61e4a67f277d6b5f44272f3389ff3a192f5f5
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-11-17 11:15:33 +0530

    Create 62. Unique Paths.cpp

---
+++ LeetCode/62. Unique Paths.cpp
@@ -0,0 +1,11 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        long ans = 1;
+        for(int i = m+n-2, j = 1; i >= max(m, n); i--, j++) 
+            ans = (ans * i) / j;
+        return ans;
+    }
+};
