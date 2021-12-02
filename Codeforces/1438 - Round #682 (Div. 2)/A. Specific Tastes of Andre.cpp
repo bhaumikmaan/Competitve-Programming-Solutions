13e4a23b0d4e81ee0acd04e9a1b1f276a024f8c0
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-02 10:00:08 +0530

    Create A. Specific Tastes of Andre.cpp

---
+++ Codeforces/1438 - Round #682 (Div. 2)/A. Specific Tastes of Andre.cpp
@@ -0,0 +1,23 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll  
+using namespace std ;
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+	while( testcases -- )
+    {
+    	int n ;
+    	cin >> n ;
+        for(int i = 1 ; i <= n ; i++)
+        {
+        	cout << 1 << " " ;
+        }
+        cout << endl ;
+    }
+    return 0 ;
+}    
