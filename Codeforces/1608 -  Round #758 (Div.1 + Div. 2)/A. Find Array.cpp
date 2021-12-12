ed73ac2ebac6a7a817041a019f46e92e812310e5
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-12 18:11:07 +0530

    Create A. Find Array.cpp

---
+++ Codeforces/1608 -  Round #758 (Div.1 + Div. 2)/A. Find Array.cpp
@@ -0,0 +1,34 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 100007
+using namespace std ;
+void solve()
+{
+	int n ;
+	cin >> n ;
+	int x = 2 ;
+	for(int i = 1 ; i <= n ; i++)
+	{
+		cout << x << " " ;
+		x++ ;
+	}
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve( ) ;
+        cout << "\n" ;
+    }	
+    return 0 ;
+}    	
