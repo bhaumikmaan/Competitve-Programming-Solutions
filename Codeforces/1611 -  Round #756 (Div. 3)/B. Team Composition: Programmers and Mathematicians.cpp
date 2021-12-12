ac15377c4010023c755ff93615bd06fa338e563b
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-12 18:17:00 +0530

    Create B. Team Composition: Programmers and Mathematicians.cpp

---
+++ Codeforces/1611 -  Round #756 (Div. 3)/B. Team Composition: Programmers and Mathematicians.cpp
@@ -0,0 +1,29 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 10000007
+using namespace std ;
+void solve()
+{
+	int x , y ;
+	cin >> x >> y ;
+	cout << min({x , y , (x + y)/4}) ;
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+    while( testcases -- )
+    {
+    	solve( ) ;
+        cout << "\n" ;
+    }	
+    return 0 ;
+}    
