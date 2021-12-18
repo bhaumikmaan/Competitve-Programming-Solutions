c2cffd4d616cf040aa8edd549a5fcd88a7ca0afb
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-18 12:30:51 +0530

    Create 902. Numbers At Most N Given Digit Set.cpp

---
+++ LeetCode/902. Numbers At Most N Given Digit Set.cpp
@@ -0,0 +1,26 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    int atMostNGivenDigitSet(vector<string>& digits, int n) {
+        string upperLimit = to_string(n) ;
+        int digit = upperLimit.size() , digitsize = digits.size() , result = 0 ;
+        for(int i = 1 ; i < digit ; i++) 
+            result += pow(digitsize , i) ;
+        
+        for(int i = 0 ; i < digit ; i++)
+        {
+            bool flag = false;
+            for(string &d :digits)
+            {
+                if(d[0] < upperLimit[i]) 
+                    result += pow(digitsize,digit -i - 1) ;
+                else if(d[0] == upperLimit[i]) 
+                    flag = true ;
+            }
+            if(!flag) 
+                return result ;
+        }
+        return result + 1 ;
+    }
+};
