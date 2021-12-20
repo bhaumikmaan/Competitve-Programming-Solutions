53255addadb9d710f4eb9579e2ae91d23c255029
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-20 10:04:06 +0530

    Create 1200. Minimum Absolute Difference.cpp

---
+++ LeetCode/1200. Minimum Absolute Difference.cpp
@@ -0,0 +1,20 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        pair<int,vector<vector<int>>> result(INT_MAX,{});
+        for(int i=0;i<arr.size()-1;++i) 
+        {
+            if(arr[i+1]-arr[i]<=result.first)
+            {
+                if(arr[i+1]-arr[i]==result.first) 
+                    result.second.push_back({arr[i],arr[i+1]});
+                else 
+                    result={arr[i+1]-arr[i],{{arr[i],arr[i+1]}}};   
+            } 
+        }
+        return result.second;
+    }
+};
