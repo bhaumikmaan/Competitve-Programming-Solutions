13c9c5410d16da6e2f0e48ddcc71d6ee099466e6
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-21 09:14:58 +0530

    Create 231. Power of Two.java

---
+++ LeetCode/231. Power of Two.java
@@ -0,0 +1,15 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0)
+        {
+            return false ;
+        }
+        if((n&(n - 1)) == 0)
+        {
+            return true ;
+        }
+        return false ;
+    }
+}
