0a6963bcf220bb9e00077c0c344acec5d332289f
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-28 09:10:36 +0530

    Create 876. Middle of the Linked List.py

---
+++ LeetCode/876. Middle of the Linked List.py
@@ -0,0 +1,13 @@
+### KNOWLEDGE IS POWER  ###
+
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
