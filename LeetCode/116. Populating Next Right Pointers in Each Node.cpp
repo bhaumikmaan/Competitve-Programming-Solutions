d0c2ac4d78767183950d633f79e2d6fe11be27a8
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2021-12-29 10:06:58 +0530

    Create 116. Populating Next Right Pointers in Each Node.cpp

---
+++ LeetCode/116. Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,16 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root) 
+            return NULL;
+        if(root->left) 
+            root->left->next = root->right;
+        if(root->right && root->next)
+            root->right->next = root->next->left;
+        connect(root->left);
+        connect(root->right);
+        return root;
+    }
+};
