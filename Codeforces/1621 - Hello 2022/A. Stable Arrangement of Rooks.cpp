f6f13f2092f9820807acbfaff0a2628b007df6dd
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-01-05 18:12:52 +0530

    Create A. Stable Arrangement of Rooks.cpp

---
+++ Codeforces/1621 - Hello 2022/A. Stable Arrangement of Rooks.cpp
@@ -0,0 +1,54 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 1000001
+using namespace std ;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi ;
+void solve()
+{
+	int n , k ;
+	cin >> n >> k ;
+	if(k > (n + 1)/2)
+	{
+		cout << -1 << "\n" ;
+	}
+	else
+	{
+		int cnt = 0 ;
+		for(int i = 0 ; i < n ; i++)
+		{
+			for(int j = 0 ; j < n ; j++)
+			{
+				if(i%2 == 0 && i == j && cnt < k)
+				{
+					cout << "R" ;
+					cnt++ ;
+				}
+				else
+				{
+					cout << "." ;
+				}
+			}
+			cout << "\n" ;
+		}
+	}
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;
+    cin >> testcases ;
+	while( testcases -- )
+    {
+    	solve( ) ;
+        // cout << "\n" ;
+    }	
+    return 0 ;
+}    	
