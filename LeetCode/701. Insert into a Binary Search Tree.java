2d39e14d051a12bea71f3fc12e2a852372cd8ce5
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-01-12 10:49:20 +0530

    Create 701. Insert into a Binary Search Tree.java

---
+++ LeetCode/701. Insert into a Binary Search Tree.java
@@ -0,0 +1,13 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) 
+            return new TreeNode(val);
+        if(root.val > val) 
+            root.left = insertIntoBST(root.left, val);
+        else 
+            root.right = insertIntoBST(root.right, val);
+        return root ;
+    }
+}
