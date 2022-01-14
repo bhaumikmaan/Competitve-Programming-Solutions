5b2e79386cf792c3db6a1dbe33918bc3f13e4e8c
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-01-14 10:01:23 +0530

    Create 8. String to Integer (atoi).cpp

---
+++ LeetCode/8. String to Integer (atoi).cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int res = 0 , i = 0 , sign = 1 ;
+        while(i < s.size() && s[i] == ' ')
+            i++ ;
+        if(s[i] == '-' || s[i] == '+')
+        {
+            if(s[i] == '-')
+            {
+                sign = -1 ;
+            }
+            else
+            {
+                sign = 1 ;
+            }
+            i++ ;
+        }
+        while(i < s.length() && (s[i] >= '0' && s[i] <= '9'))
+        {
+            int digit = (s[i]-'0')*sign ;
+            if(sign == 1 && (res > INT_MAX/10 || (res == INT_MAX/10 && digit > INT_MAX%10)))
+                return INT_MAX ;
+            if(sign == -1 &&(res < INT_MIN/10 || (res == INT_MIN/10 && digit < INT_MIN%10)))
+                return INT_MIN ;
+            res = res*10 + digit ;
+            i++ ;
+        }
+        return res ;
+    }
+};
