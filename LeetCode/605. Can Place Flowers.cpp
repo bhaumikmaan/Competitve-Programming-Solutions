fb606758d8fbcda2cc1c31b35fc33c10c3e0ac5a
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-01-18 09:21:09 +0530

    Create 605. Can Place Flowers.cpp

---
+++ LeetCode/605. Can Place Flowers.cpp
@@ -0,0 +1,37 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0 ;
+        for(int i = 0 ; i < flowerbed.size(); i++)
+        {
+            if(flowerbed[i] == 0)
+            {
+                int prev , next ;
+                if(i == 0 || flowerbed[i - 1] == 0)
+                {
+                    prev = 0 ;
+                }
+                else
+                {
+                    prev = 1 ;
+                }
+                if(i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)
+                {
+                    next = 0 ;
+                }
+                else
+                {
+                    next = 1 ;
+                }
+                if(prev == 0 && next == 0)
+                {
+                    flowerbed[i] = 1 ;
+                    count++ ;
+                }
+            }
+        }
+        return count >= n ;
+    }
+};
