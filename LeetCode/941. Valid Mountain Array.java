14b8c3fcf2950aae3e608ceeb529806cf4e4a880
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-01-25 10:21:09 +0530

    Create 941. Valid Mountain Array.java

---
+++ LeetCode/941. Valid Mountain Array.java
@@ -0,0 +1,14 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) 
+            return false;
+        int l = 0 , r = arr.length - 1 ;
+        while(l + 1 < arr.length - 1 && arr[l] < arr[l + 1]) 
+            l++ ;
+        while(r - 1 > 0 && arr[r] < arr[r - 1]) 
+            r-- ;
+        return l == r;
+    }
+}
