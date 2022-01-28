88c1ac18cefb3f783c860aad455399327a2cc3e3
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-01-28 09:16:32 +0530

    Create 211. Design Add and Search Words Data Structure.cpp

---
+++ LeetCode/211. Design Add and Search Words Data Structure.cpp
@@ -0,0 +1,38 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class WordDictionary
+{
+     unordered_map<int, vector<string>> words ;
+     bool isEqual(string word1, string word2)
+     {
+          for (int i = 0; i < word1.size() ; i++)
+          {
+               if (word2[i] == '.')
+                    continue ;
+               if (word1[i] != word2[i])
+                    return false ;
+          }
+          return true ;
+     }
+
+public:
+     WordDictionary() {}
+     void addWord(string word)
+     {
+          words[word.size()].push_back(word) ;
+     }
+     bool search(string word)
+     {
+          for (auto &&s : words[word.size()])
+               if (isEqual(s, word))
+                    return true ;
+          return false ;
+     }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
