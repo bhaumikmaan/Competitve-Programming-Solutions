ef6ae6cc8b83ed087996aeda1ae8d8858d2537c0
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-02-15 09:57:31 +0530

    Create 136. Single Number.py

---
+++ LeetCode/136. Single Number.py
@@ -0,0 +1,8 @@
+###  KNOWLEDGE IS POWER  ###
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans = 0
+        for i in nums:
+            ans ^= i
+        return ans 
