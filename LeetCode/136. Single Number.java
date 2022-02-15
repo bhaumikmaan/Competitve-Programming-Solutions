6b1bd4c8ee6b5d07a6f2031d0e5a62a24667e0d0
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-02-15 09:52:48 +0530

    Create 136. Single Number.java

---
+++ LeetCode/136. Single Number.java
@@ -0,0 +1,15 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums) ;
+        for(int i = 1 ; i < nums.length ; i += 2)
+        {
+            if(nums[i] != nums[i - 1])
+            {
+                return nums[i-1] ;
+            }
+        }
+        return nums[nums.length - 1] ;
+    }
+}
