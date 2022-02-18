9895763cc5a16d26279d47b0b13d4816d01c5280
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-02-18 19:52:37 +0530

    Create Control the Pollution.cpp

---
+++ Codechef/START26B - Starters 26 Div. 3/Control the Pollution.cpp
@@ -0,0 +1,35 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 100010
+using namespace std ;
+typedef vector<int> vi ; 
+typedef vector<string> vs ;
+typedef vector<vector<int>> vvi ;
+#define haan cout << "YES"
+void solve()
+{
+	int n , x , y ;
+	cin >> n >> x >> y ;
+	int car = (ceil(float(n)/4)) , bus = (ceil(float(n)/100)) ;
+	int a = (floor(float(n)/100))*x + (ceil(float(n%100)/4))*y ;
+	cout << min(y*car , min(a , x*bus)) ;
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;		
+    cin >> testcases ;	
+    while( testcases -- )	
+    {
+    	solve( ) ;
+    	cout << "\n" ;
+    }
+    return 0 ;
+}        	
