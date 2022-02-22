afcef4845040d97c4fd682043c330b5831098d14
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-02-22 21:53:39 +0530

    Create 171. Excel Sheet Column Number.java

---
+++ LeetCode/171. Excel Sheet Column Number.java
@@ -0,0 +1,12 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int ans = 0;
+        for(int i = 0; i < columnTitle.length(); i++)
+        {
+            ans = ans * 26 + columnTitle.charAt(i) - 'A' + 1 ;
+        }
+        return ans ;
+    }
+}
