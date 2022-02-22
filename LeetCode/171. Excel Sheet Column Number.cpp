9410bb4b5033da752e45060654c92a7134fdd226
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-02-22 21:53:04 +0530

    Create 171. Excel Sheet Column Number.cpp

---
+++ LeetCode/171. Excel Sheet Column Number.cpp
@@ -0,0 +1,14 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int result = 0 ;
+        for(char c : columnTitle)
+        {
+            int d = c - 'A' + 1 ;
+            result = result * 26 + d ;
+        }
+        return result ;
+    }
+};
