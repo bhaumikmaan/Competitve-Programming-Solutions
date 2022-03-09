b817c32999656ff0fd9a9aa4ef941effa70ad25a
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-03-09 18:55:12 +0530

    Create B. DIV + MOD.cpp

---
+++ Codeforces/1650 - Round #776 (Div. 3)/B. DIV + MOD.cpp
@@ -0,0 +1,40 @@
+/***  KNOWLEDGE IS POWER  ***/
+
+#include<bits/stdc++.h>
+typedef long long ll ;
+#define int ll 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define mod 1000000007
+#define inf LLONG_MAX
+#define N 100010
+using namespace std ;
+typedef vector<int> vi ; 
+typedef vector<string> vs ;
+typedef vector<vector<int>> vvi ;
+typedef vector<pair<int,int>> vpi ;
+#define haan cout << "YES"
+void solve()
+{
+	int l , r , a ;
+	cin >> l >> r >> a ;
+	int ans = r/a + r%a ;
+	int x = (r/a)*a - 1 ;
+	if(x >= l)
+	{
+		ans = max(ans , x/a + x%a) ;
+	}
+	cout << ans ;
+}
+signed main( )
+{
+    ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+    int testcases = 1 ;		
+    cin >> testcases ;	
+    while( testcases -- )	
+    {
+    	solve( ) ;
+    	cout << "\n" ;	
+    }
+    return 0 ;
+}        	
