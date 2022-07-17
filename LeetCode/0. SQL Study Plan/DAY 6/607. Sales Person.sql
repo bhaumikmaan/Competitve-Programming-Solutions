8e4fa1d8a3d5d69536dfcc077357fe9ddc94aaf6
Bhaumik Maan <82998871+bhaumikmaan@users.noreply.github.com>
2022-07-17 16:16:47 +0530

    Create 607. Sales Person.sql

---
+++ LeetCode/0. SQL Study Plan/DAY 6/607. Sales Person.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+SELECT name FROM salesperson WHERE sales_id NOT IN (SELECT sales_id FROM orders WHERE com_id IN (SELECT com_id FROM company WHERE name = 'RED')) ;
